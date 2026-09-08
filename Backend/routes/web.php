cdafe44db0f4f65f635d9ac5cb9f5920f902b4f7
Amine Sakhri <sakhriamine2003@gmail.com>
2026-09-08 10:46:14 +0100

    creation les megratione

--- Backend/routes/web.php
+++ Backend/routes/web.php
@@ -7,11 +7,3 @@ Route::get('/', function () {
     return Inertia::render('welcome');
 })->name('home');
 
-Route::middleware(['auth'])->group(function () {
-    Route::get('dashboard', function () {
-        return Inertia::render('dashboard');
-    })->name('dashboard');
-});
-
-require __DIR__.'/settings.php';
-require __DIR__.'/auth.php';
