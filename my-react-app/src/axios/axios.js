4a1a78ad5f7a4f973dd7f57bde5b956da980dcd9
Amine Sakhri <sakhriamine2003@gmail.com>
2026-09-26 11:35:24 +0000

    Use current host for API on network

--- my-react-app/src/axios/axios.js
+++ my-react-app/src/axios/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const apiHost = window.location.hostname || "localhost";
+
 const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: `http://${apiHost}:8000/api`,
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
